bb131ad0e35feb6547bbb690364b40d01830ca12
FaithBook233 <1203339302@qq.com>
2023-01-15 01:34:30 +0800

    init: 初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# Game Server Manager
+
+用于管理UE4服务器的Linux程序
---
+++ Source/GameServerManager/GameServerManagerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameServerManagerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMESERVERMANAGER_API AGameServerManagerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
